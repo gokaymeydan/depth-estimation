4e9ad3d0620cbee20a5eaa83a6730187a1c3115b
Gökay Meydan <meygokay@icloud.com>
2025-07-19 22:24:09 +0200

    add api functionality

--- cli.py
+++ cli.py
@@ -4,15 +4,15 @@ from predictor import DepthEstimationModel
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Depth estimation using ZoeDepth.')
-    parser.add_argument("input_image",help='Path to input image.')
-    parser.add_argument("output_image",help='Path to output depth map.')
+    parser = argparse.ArgumentParser(description="Depth estimation using ZoeDepth.")
+    parser.add_argument("input_image", help="Path to input image.")
+    parser.add_argument("output_image", help="Path to output depth map.")
     args = parser.parse_args()
 
     model = DepthEstimationModel()
     result = model.calculate_depthmap(args.input_image, args.output_image)
     print(result)
 
+
 if __name__ == "__main__":
     main()
-    
